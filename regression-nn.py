5124c837e147ad3180201225afef96c3788ffdb3
Paul Cuciureanu <paulcu@gmail.com>
2019-02-03 23:21:41 -0500

    feat: use a nn for gresssion

---
+++ regression-nn.py
@@ -0,0 +1,5 @@
+#%%
+import pandas as pd
+
+#%%
+automobile_data = pd.read_csv('C:/Users/pcuci/Downloads/pytorch-building-deep-learning-models/datasets/Automobile_data.csv', sep=r'\s*,\s*', engine='python')
