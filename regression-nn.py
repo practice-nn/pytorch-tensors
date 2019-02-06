d1964fcd01ec0883a8a874702710e6a767e60e4f
Paul Cuciureanu <paulcu@gmail.com>
2019-02-06 12:08:03 -0500

    feat: prep for regression nn

--- regression-nn.py
+++ regression-nn.py
@@ -40,6 +40,19 @@ automobile_target = automobile_target.astype(float)
 automobile_target['price'].describe()
 
 #%%
-automobile_features = pd.get_dummies(automobile_features, columns=['make', 'fuel-type', 'body-style'])
+automobile_features = pd.get_dummies(automobile_features, columns=['make', 'fuel-type', 'body-style']) # one-hot encoding for non-numeric values
+automobile_features.head()
+
+#%%
+automobile_features.columns
+
+#%%
+from sklearn import preprocessing
+automobile_features[['horsepower']] = preprocessing.scale(automobile_features[['horsepower']]) # standardize the numeric values: subtract mean, divide by standard deviation
+automobile_features[['horsepower']].head() # all ML algorithms work better when the numeric values are standardized to be roughly in the same range
+
+#%%
+from sklearn.model_selection import train_test_split
+X_train, x_test, Y_train, y_test = train_test_split(automobile_features, automobile_target, test_size=0.2, random_state=0) # use 80% of the data for training purposes
 
 #%%
