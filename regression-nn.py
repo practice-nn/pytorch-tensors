6cd485573434351c3c6d526015a9ba219cb9cb3f
Paul Cuciureanu <paulcu@gmail.com>
2019-02-06 12:38:20 -0500

    feat: add price predictor nn with 100 hidden neurons

--- regression-nn.py
+++ regression-nn.py
@@ -56,3 +56,79 @@ from sklearn.model_selection import train_test_split
 X_train, x_test, Y_train, y_test = train_test_split(automobile_features, automobile_target, test_size=0.2, random_state=0) # use 80% of the data for training purposes
 
 #%%
+import torch
+dtype = torch.float
+
+#%%
+X_train_tensor = torch.tensor(X_train.values, dtype = dtype)
+x_test_tensor = torch.tensor(x_test.values, dtype = dtype)
+X_train_tensor.shape
+
+#%%
+Y_train_tensor = torch.tensor(Y_train.values, dtype = dtype)
+y_test_tensor = torch.tensor(y_test.values, dtype = dtype)
+Y_train_tensor.shape
+
+#%%
+inp = 26 # the 26 features in X_train_tensor
+out = 1 # a single value output, the price
+
+hid = 100 # hidden layer neurons
+
+loss_fn = torch.nn.MSELoss() # use the torch.nn library to not calculate the loss by hand as in the previous example
+
+learning_rate = 0.0001
+
+#%%
+model = torch.nn.Sequential(torch.nn.Linear(inp, hid), torch.nn.Sigmoid(), torch.nn.Linear(hid, out)) # a sequential model holding NN layers in sequence; all neural network classes derive from the base torch.nn.Module class; any layer can contain a nested module; the Sigmoid is our choice for the activation function
+
+#%%
+for iter in range(10000): # 10K epochs, or passes through our network
+    y_pred = model(X_train_tensor) # apply the model to the input training data
+    loss = loss_fn(y_pred, Y_train_tensor)
+    if iter % 1000 == 0:
+        print(iter, loss.item())
+    model.zero_grad() # zero out the model gradients before backpropagation
+    loss.backward()
+    with torch.no_grad(): # don't calculate gradients while we're updating our model's parameters
+        for param in model.parameters(): # accesses all params from the nn
+            param -= learning_rate * param.grad
+
+
+#%%
+# take a sample from our test data and perform a prediction
+sample = x_test.iloc[23]
+sample
+
+#%%
+sample_tensor = torch.tensor(sample.values, dtype = dtype) # convert to tensor
+sample_tensor
+
+
+#%%
+y_pred = model(sample_tensor) # pass the sample tensor through our nn model
+print("Predicted price of automible is: ", int(y_pred.item()))
+print("Actual price of automible is: ", int(y_test.iloc[23]))
+
+#%%
+# now run predictions on the entire test dataset
+y_pred_tensor = model(x_test_tensor)
+y_pred = y_pred_tensor.detach().numpy() # for visualisation
+plt.scatter(y_pred, y_test.values)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+plt.show()
+
+#%%
+# serialize the model to disk
+torch.save(model, 'my_model')
+saved_model = torch.load('my_model')
+y_pred_tensor = saved_model(x_test_tensor)
+y_pred = y_pred_tensor.detach().numpy()
+plt.figure(figsize=(15, 6))
+plt.plot(y_pred, label='Predicted Price')
+plt.plot(y_test.values, label='Actual Price')
+plt.legend()
+plt.show()
+
+#%%
